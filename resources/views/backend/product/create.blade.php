4e6d5c8d1c0f9b6c34930f22a5d710d09f464451
Md Al Amin Sarker <53163466+alamin-php@users.noreply.github.com>
2021-12-07 14:40:53 +0600

    product edit done

--- resources/views/backend/product/create.blade.php
+++ resources/views/backend/product/create.blade.php
@@ -28,101 +28,113 @@
               @csrf
                 <div class="box-body">
                   <div class="row">
-                    <div class="col-md-12">
-                      <div class="form-group">
-                        <label for="inputproductName"> {{ __('Product name') }}</label>
-                        <input type="text" name="name" class="form-control" id="inputproductName" placeholder="Enter a new product name">
-                        @error('name')
-                          <span class="invalid-feedback text-danger" role="alert">
+                    <div class="col-md-9">
+                      <div class="row">
+                        <div class="col-md-4">
+                          <div class="form-group">
+                            <label for="inputproductName"> {{ __('Product name') }}</label>
+                            <input type="text" name="name" class="form-control" id="inputproductName" placeholder="Enter a new product name">
+                            @error('name')
+                              <span class="invalid-feedback text-danger" role="alert">
+                              <strong>{{ $message }}</strong>
+                              </span>
+                            @enderror
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <div class="form-group">
+                            <label for="inputproductName"> {{ __('Product category') }}</label>
+                            <select name="cat_id" id="" class="form-control">
+                            <option value="" disabled selected>==Choose One==</option>
+                            @foreach($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+                            @endforeach
+                            </select>
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <div class="form-group">
+                            <label for="inputproductName"> {{ __('Product brand') }}</label>
+                            <select name="brand_id" id="" class="form-control">
+                            <option value="" disabled selected>==Choose One==</option>
+                            @foreach($brands as $brand)
+                            <option value="{{ $brand->id }}">{{ $brand->brand_name }}</option>
+                            @endforeach
+                            </select>
+                            </div>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-4">
+                          <div class="form-group">
+                            <label for="inputproductName"> {{ __('Product price') }}</label>
+                            <input type="number" name="price" class="form-control" placeholder="Enter product price">
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <div class="form-group">
+                            <label for="inputproductName"> {{ __('Product size') }}</label>
+                            <input type="text" name="size" class="form-control" placeholder="Enter product size">
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <div class="form-group">
+                            <label for="inputproductName"> {{ __('Product color') }}</label>
+                            <input type="text" name="color" class="form-control" placeholder="Enter product color">
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row">
+                          <div class="col-md-12">
+                            <div class="form-group">
+                            <label for="short_description"> {{ __('Short discription') }}</label>
+                            <textarea class="textarea" name="short_description" placeholder="Enter product short description"
+                            style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+                            @error('short_description')
+                            <span class="invalid-feedback text-danger" role="alert">
                             <strong>{{ $message }}</strong>
-                          </span>
-                        @enderror
-                      </div>
-                    </div>
-                  </div>
-                <div class="row">
-                    <div class="col-md-6">
-                      <div class="form-group">
-                      <label for="inputproductName"> {{ __('Product category') }}</label>
-                        <select name="cat_id" id="" class="form-control">
-                          <option value="" disabled selected>==Choose One==</option>
-                          @foreach($categories as $category)
-                          <option value="{{ $category->id }}">{{ $category->category_name }}</option>
-                          @endforeach
-                        </select>
-                      </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="form-group">
-                      <label for="inputproductName"> {{ __('Product brand') }}</label>
-                        <select name="brand_id" id="" class="form-control">
-                          <option value="" disabled selected>==Choose One==</option>
-                          @foreach($brands as $brand)
-                          <option value="{{ $brand->id }}">{{ $brand->brand_name }}</option>
-                          @endforeach
-                        </select>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-md-12">
-                      <div class="form-group">
-                        <label for="short_description"> {{ __('Short discription') }}</label>
-                          <textarea class="textarea" name="short_description" placeholder="Enter product short description"
-                          style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
-                        @error('short_description')
-                          <span class="invalid-feedback text-danger" role="alert">
+                            </span>
+                            @enderror
+                          </div>
+                          <div class="form-group">
+                            <label for="long_description"> {{ __('Long discription') }}</label>
+                            <textarea class="textarea" name="long_description" placeholder="Enter product long description"
+                            style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+                            @error('long_description')
+                            <span class="invalid-feedback text-danger" role="alert">
                             <strong>{{ $message }}</strong>
-                          </span>
-                        @enderror
+                            </span>
+                            @enderror
+                          </div>
+                        </div>
                       </div>
-                      <div class="form-group">
-                        <label for="long_description"> {{ __('Long discription') }}</label>
-                          <textarea class="textarea" name="long_description" placeholder="Enter product long description"
-                          style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
-                        @error('long_description')
-                          <span class="invalid-feedback text-danger" role="alert">
-                            <strong>{{ $message }}</strong>
-                          </span>
-                        @enderror
-                      </div>
+                    </div>
+                    <div class="col-md-3">
+                     <div class="row">
+                        <div class="col-md-12">
+                          <div class="form-group">
+                          <label for="inputproductName"> {{ __('Image') }}</label>
+                          <input type="file" name="image" id="inputImage" accept="iamge/*" class="upload" onchange="readURL(this);">
+                          </div>
+                        </div>
+                     </div>
+                     <div class="row">
+                        <div class="col-md-12">
+                        <img class="img-fulid" id="image" src="" alt="Upload product image">
+                        </div>
+                     </div>
+                     <div class="row">
+                       <div class="col-md-12">
+                        <div class="checkbox">
+                          <label>
+                          <input type="checkbox" name="status" value="1">
+                          This product will be publish?
+                          </label>
+                        </div>
+                       </div>
+                     </div>
                     </div>
                   </div>
-                  <div class="row">
-                    <div class="col-md-4">
-                      <div class="form-group">
-                      <label for="inputproductName"> {{ __('Product price') }}</label>
-                        <input type="number" name="price" class="form-control" placeholder="Enter product price">
-                      </div>
-                    </div>
-                    <div class="col-md-4">
-                      <div class="form-group">
-                        <label for="inputproductName"> {{ __('Product size') }}</label>
-                        <input type="text" name="size" class="form-control" placeholder="Enter product size">
-                      </div>
-                    </div>
-                    <div class="col-md-4">
-                      <div class="form-group">
-                        <label for="inputproductName"> {{ __('Product color') }}</label>
-                        <input type="text" name="color" class="form-control" placeholder="Enter product color">
-                      </div>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-md-6">
-                      <div class="form-group">
-                      <label for="inputproductName"> {{ __('Image') }}</label>
-                        <input name="image" type="file" >
-                      </div>
-                      <div class="checkbox">
-                    <label>
-                      <input type="checkbox" name="status" value="1">
-                      This product will be publish?
-                    </label>
-                  </div>
-                    </div>
-                  </div>
-                </div>
                 <div class="box-footer">
                 <a href="{{ route('product.index') }}" type="button" class="btn btn-default btn-sm">{{ __('Back') }}</a>
                 <button type="submit" class="btn btn-primary btn-sm pull-right">{{ __('Create product') }}</button>
@@ -142,4 +154,17 @@
     $('.textarea').wysihtml5()
   })
 </script>
+<script type="text/javascript">
+        function readURL(input){
+            if(input.files && input.files[0]){
+                var reader = new FileReader();
+                reader.onload = function (e){
+                    $('#image')
+                        .attr('src', e.target.result)
+                        .width(250)
+                };
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+    </script>
 @endpush
